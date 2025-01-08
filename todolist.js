8ad675c96fc119d4f05cf887828202d305011295
Rednasrot <rednasrot@gmail.com>
2025-01-08 12:31:19 +0100

    UPDATE | Oppdatering v0.2 | Endret skriveleif

--- todolist.js
+++ todolist.js
@@ -2,31 +2,27 @@ document.getElementById('add').addEventListener('click', function() {
   let inputValue = document.getElementById('to-do').value;
   if (inputValue) {
     addTodo(inputValue);
-    document.getElementById('to-do').value = " ";
+    document.getElementById('to-do').value = "";
   }
 });
 
 function addTodo(task) {
- let todoList = document.getElementById ('list')
- let li = document.createElement('li');
-//  li.innerHTML = `<span>${todoList.text}</span>
-//  <button data-id"${todo.id}> Remove </button>`
- li.textContent = task;
- 
- let removeButton = document.createElemement ('button')
- removeButton.textcontent = 'Remove';
- removeButton.onclick = function() {
-  todoList.removeChild(li);
- };
+  let todoList = document.getElementById('list');
+  let li = document.createElement('li');
+  // li.innerHTML = `<span>${todoList.text}</span>
+  // <button data-id="${todo.id}">Remove</button>`;
+  li.textContent = task;
 
+  let removeButton = document.createElement('button');
+  removeButton.textContent = 'Remove';
+  removeButton.onclick = function() {
+    todoList.removeChild(li);
+  };
 
- li.appendChild(removeButton);
- li.addEventListener('click', function() {
-  li.classlist.toggle('completed');
+  li.appendChild(removeButton);
+  li.addEventListener('click', function() {
+    li.classList.toggle('completed');
   });
 
-  todoList.appendchild(li);
-
- };
-
-
+  todoList.appendChild(li);
+}
