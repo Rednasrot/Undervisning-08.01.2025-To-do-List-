3e9fb2423b7e83e41b4e55e1998f52f51d7a2758
Rednasrot <rednasrot@gmail.com>
2025-01-08 13:32:04 +0100

    Oppdatering | V 0.002 | Funker halveis

--- todolist.js
+++ todolist.js
@@ -2,16 +2,24 @@ document.getElementById('add').addEventListener('click', function() {
   let inputValue = document.getElementById('to-do').value;
   if (inputValue) {
     addTodo(inputValue);
+    saveTodoToLocalStorage(inputValue, false);
     document.getElementById('to-do').value = "";
   }
 });
 
-function addTodo(task) {
+
+function addTodo(item, isCompleted = false) {
   let todoList = document.getElementById('list');
   let li = document.createElement('li');
   // li.innerHTML = `<span>${todoList.text}</span>
   // <button data-id="${todo.id}">Remove</button>`;
-  li.textContent = task;
+  li.textContent = item;
+
+  if (isCompleted) {
+    li.classList.add("Completed")
+
+
+  };
 
   let removeButton = document.createElement('button');
   removeButton.textContent = 'Remove';
@@ -25,4 +33,19 @@ function addTodo(task) {
   });
 
   todoList.appendChild(li);
-}
+};
+
+
+//local storage:
+
+
+document.addEventListener("DOMContentLoaded", function() {
+ let savedItems = JSON.parse(localStorage.getItem("items")) || [];
+ savedItems.array.forEach(item => addItem (item.text, item.completed));
+});
+
+function saveTodoToLocalStorage(item, isCompleted) {
+ const todos = JSON.parse(localStorage.getItem('todos')) || [];
+ todos.push({ text: item, completed: isCompleted})
+ localStorage.setItem('todos', JSON.stringify(todos));
+};
